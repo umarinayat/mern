8521c04a4481cc37d979b04687ea6858abb07b0e
Umar Inayat <umarinayat111@gmail.com>
2022-07-24 01:25:53 +0500

    Front End added

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,34 @@
+import { useEffect } from "react";
+
+import WorkoutDetails from "../components/WorkoutDetails";
+import WorkoutForm from "../components/WorkoutForm";
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
+
+const Home = () => {
+  const { workouts, dispatch } = useWorkoutsContext();
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const res = await fetch("/api/workouts");
+      const json = await res.json();
+
+      if (res.ok) {
+        dispatch({ type: "SET_WORKOUTS", payload: json });
+      }
+    };
+
+    fetchWorkouts();
+  }, [dispatch]);
+  return (
+    <div className="home">
+      <div className="workouts">
+        {workouts &&
+          workouts.map((workout) => (
+            <WorkoutDetails key={workout._id} workout={workout} />
+          ))}
+      </div>
+      <WorkoutForm />
+    </div>
+  );
+};
+
+export default Home;
